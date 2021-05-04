9419bad9f9bb9f0649f9ad02332cc2f632b0ae92
Adam Gibbs <gibbsadamg@gmail.com>
2021-05-04 17:31:12 -0400

    create report.

--- src/test2.sh
+++ src/test2.sh
@@ -4,7 +4,7 @@ for i in 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19
 do
 	for j in 5000 10000 20000 30000 40000
 	do
-		echo "Run $i for sample size $j"
+		echo "Improved run $i for sample size $j"
 		java Main -i $j ../input/ca-AstroPh.txt > ~/amherst/cosc254/cosc254-triangle-counting/output/output.${j}I.$i.txt &
 	done
 done
